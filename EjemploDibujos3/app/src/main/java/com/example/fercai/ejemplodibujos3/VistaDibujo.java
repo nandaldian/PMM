a994c9430d343741aea1077522540227102fbdfc
nandaldian <pkt1233123@gmail.com>
2017-11-03 13:56:13 +0100

    Actualizado ejemploDibujo2-3

--- EjemploDibujos3/app/src/main/java/com/example/fercai/ejemplodibujos3/VistaDibujo.java
+++ EjemploDibujos3/app/src/main/java/com/example/fercai/ejemplodibujos3/VistaDibujo.java
@@ -1,10 +1,40 @@
 package com.example.fercai.ejemplodibujos3;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+
 /**
  * Created by fercai on 3/11/17.
  */
 
-public class VistaDibujo {
+public class VistaDibujo extends View {
+    private ShapeDrawable miDrawable;
 
-    
+    public VistaDibujo(Context c) {super(c);}
+    public VistaDibujo(Context c, AttributeSet attrs){
+        super(c, attrs);
+    }
+    protected void onDraw(Canvas lienzo) {
+        Paint miPincel = new Paint();
+        miPincel.setColor(Color.RED);
+        miPincel.setStyle(Paint.Style.STROKE);
+        lienzo.drawCircle(500, 500, 300, miPincel);
+
+        miPincel.setTextSize(60);
+        lienzo.drawText("MI CIRCULO", 500, 900, miPincel);
+        String mensaje = "Ancho: " + getWidth() + " Alto: " + getHeight();
+        lienzo.drawText(mensaje, 500, 1000, miPincel);
+    }
 }
